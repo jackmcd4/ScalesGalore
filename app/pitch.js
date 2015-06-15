1aee87a2dca87effee6c20e2d1af0bf44e3a020d
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-06-15 12:41:08 -0700

    Have Sound! (sort of)

--- app/pitch.js
+++ app/pitch.js
@@ -1,7 +1,11 @@
-var parse = require('music-parser');
+// var T = require("timbre")
 
-var T = require("timbre");
-
+var T = require(['node_modules/timbre/timbre.dev.js'], function (T) {
+    //foo is now loaded.
+    func();
+});
+var func = function(){
+    console.log(T);
 T("sin", {freq:880, mul:0.5}).play();   
 
 var BD;
@@ -10,3 +14,4 @@ T("audio").load("./drumkit.wav", function() {
     drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD).play()
     BD = this.slice(0,  500).set({bang:false})
 })
+}
