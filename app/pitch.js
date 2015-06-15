633511a83d85656945317463c23c8f521fe0aa0b
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-06-15 16:15:05 -0700

    Working on layout and attempt to add in angular

--- app/pitch.js
+++ app/pitch.js
@@ -1,17 +1,30 @@
-// var T = require("timbre")
-
 var T = require(['node_modules/timbre/timbre.dev.js'], function (T) {
     //foo is now loaded.
     func();
 });
+
 var func = function(){
-    console.log(T);
-T("sin", {freq:880, mul:0.5}).play();   
 
-var BD;
-
-T("audio").load("./drumkit.wav", function() {
-    drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD).play()
-    BD = this.slice(0,  500).set({bang:false})
+var freqs = T(function(count) {
+  return [261.6, 277.2, 293.7, 311.1, 329.6, 349.2, 370.0, 392.0, 415.3, 440.0, 466.2, 493.9][count % 12];
+});
+var osc = T("sin", {freq:freqs, mul:0.5});
+var env = T("perc", {a:50, r:500}, osc).bang();
+$('.play').on('click', function(){
+var interval = T("param", {value:500});
+T("interval", {interval:interval, timeout:"5.5sec"}, freqs, env).start();
+    env.play();  
 })
+
 }
+
+
+var theory = require(['node_modules/MUSIC/index.js'], function (T) {
+    //foo is now loaded.
+    func2();
+});
+
+var func2 = function(){
+    console.log(Note.fromLatin('A4').frequency())
+}
+
