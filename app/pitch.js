1e124d3ef2cd8dba4856138531cf16ae0aa86dbe
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-06-16 13:47:53 -0700

    All scales working, staff appended...can't get notes to change!!!

--- app/pitch.js
+++ app/pitch.js
@@ -8,7 +8,11 @@ var theory = require(['node_modules/MUSIC/index.js'], function (T) {
     //foo is now loaded.
 });
 
+// var vex = require(['node_modules/vextab/releases/vextab-div.js'], function(V){
 
+// });
+
+     
 $('.M').on('click', function(){
     var n = ($(this).context.innerHTML);
     if(n.length>1){
@@ -16,6 +20,9 @@ $('.M').on('click', function(){
     }
     n = n.concat('4');
     createScale(n, 'major');
+    $('body').append('<div class="vex-tabdiv2" width=680 scale=1.0 editor="false">options space=20 \n tabstave \n notation=true tablature=false notes C-D-E-F-G-A-B/4 C/5</div>')
+    console.log($('vex-tabdiv1').text)
+    console.log($('vex-tabdiv2'))
 })
 
 $('.Nm').on('click', function(){
@@ -36,6 +43,15 @@ $('.Hm').on('click', function(){
     createScale(n, 'harmonic minor');
 })
 
+$('.Mm').on('click', function(){
+    var n = ($(this).context.innerHTML);
+    if(n.length>1){
+        n = n.slice(0, 2);
+    }
+    n = n.concat('4');
+    createScale(n, 'melodic minor');
+})
+
 $('.mp').on('click', function(){
     var n = ($(this).context.innerHTML);
     if(n.length>1){
@@ -54,6 +70,41 @@ $('.Mp').on('click', function(){
     createScale(n, 'major pentatonic');
 })
     
+$('.Marp').on('click', function(){
+    var n = ($(this).context.innerHTML);
+    if(n.length>1){
+        n = n.slice(0, 2);
+    }
+    n = n.concat('4');
+    createScale(n, 'major arpeggio');
+})
+
+$('.marp').on('click', function(){
+    var n = ($(this).context.innerHTML);
+    if(n.length>1){
+        n = n.slice(0, 2);
+    }
+    n = n.concat('4');
+    createScale(n, 'minor arpeggio');
+})
+
+$('.Darp').on('click', function(){
+    var n = ($(this).context.innerHTML);
+    if(n.length>1){
+        n = n.slice(0, 2);
+    }
+    n = n.concat('4');
+    createScale(n, 'diminished arpeggio');
+})
+
+$('.W').on('click', function(){
+    var n = ($(this).context.innerHTML);
+    if(n.length>1){
+        n = n.slice(0, 2);
+    }
+    n = n.concat('4');
+    createScale(n, 'whole tone');
+})
 
 $('.play').on('click', function(){
     playScale([261.6, 277.2, 293.7, 311.1, 329.6, 349.2, 370.0, 392.0, 415.3, 440.0, 466.2, 493.9, 523.2])
@@ -62,9 +113,24 @@ $('.play').on('click', function(){
 
 var createScale = function(note, type){
     var n = Note.fromLatin(note);
-    var sc = n.scale(type);
+    var wholeStep = Interval.fromSemitones(2);
+    if(type.split(' ')[1] === 'tone'){
+        var sc = [n]
+        for(var i = 1; i < 7; i++){
+            sc[i] = sc[i-1].add(wholeStep)
+        }
+    }else{
+        var sc = n.scale(type);
+    }
     for(var i = 0; i < sc.length; i++){
         sc[i] = sc[i].frequency();
+    }
+    if(type.split(' ')[1] === 'arpeggio'){
+        sc.splice(sc.length-1, 1);
+    }
+    else{
+       var rev = sc.slice()
+        sc = sc.concat(rev.reverse());
     }
     playScale(sc);
     return;
@@ -81,7 +147,8 @@ var playScale = function(scale){
     var interval = T("param", {value:500});
     T("interval", {interval:interval, timeout:time}, freqs, env).start();
         env.play();  
-    }
+}
+
 
 
 }
